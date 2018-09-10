3f6e0f761340f1978fbb4d6f527d52d477ac658b
YORIS <yorisprayogo@gmail.com>
2018-09-10 15:27:03 +0700

    default action is open camera if no intent passed

--- app/src/main/java/com/yorisprayogo/amarthascanner/MainActivity.kt
+++ app/src/main/java/com/yorisprayogo/amarthascanner/MainActivity.kt
@@ -2,15 +2,21 @@ package com.yorisprayogo.amarthascanner
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import com.scanlibrary.ScanConstants
 import com.scanlibrary.ScanActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.net.URI
 
 
 class MainActivity : AppCompatActivity() {
 
     val REQUEST_CODE = 99
+    var isWithResult = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,17 +24,40 @@ class MainActivity : AppCompatActivity() {
 
         val preference = intent?.getIntExtra("amf-req-type", 4)
 
+        if(intent?.hasExtra("amf-req-type") == true) {
+            isWithResult = true
+        }
+
         val intent = Intent(this, ScanActivity::class.java)
         intent.putExtra(ScanConstants.OPEN_INTENT_PREFERENCE, preference)
         startActivityForResult(intent, REQUEST_CODE)
     }
 
+    fun getPathFromUri(uri: Uri?): File{
+        val path = uri?.path
+        return File(URI(path))
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            setResult(Activity.RESULT_OK, data)
+            if(isWithResult) {
+                setResult(Activity.RESULT_OK, data)
+            }else{
+                val uri = data?.extras?.getParcelable<Uri>("scannedResult")
+                try {
+                    val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+                    contentResolver.delete(uri, null, null)
+                    imageView.setImageBitmap(bitmap)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
         }
-        finish()
+
+        if(isWithResult){
+            finish()
+        }
     }
 
 }
